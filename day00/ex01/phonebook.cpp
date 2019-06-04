6e1ab2116736dd67c476f4e336d31118e6939ffd
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-06-04 12:39:01 +0200

    Completed ex01

--- day00/ex01/phonebook.cpp
+++ day00/ex01/phonebook.cpp
@@ -6,7 +6,7 @@
 /*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 10:01:37 by shillebr          #+#    #+#             */
-/*   Updated: 2019/06/04 09:55:52 by shillebr         ###   ########.fr       */
+/*   Updated: 2019/06/04 12:38:36 by shillebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,37 +43,37 @@ Contact		addNewContact() {
 	std::string		input;
 
 	std::cout<<"Please enter a first name: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setFirstName(input);
     std::cout<<"Please enter a last name: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setLastName(input);
     std::cout<<"Please enter a nickname: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setNickname(input);
     std::cout<<"Please enter a login: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setLogin(input);
     std::cout<<"Please enter a postal address: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setPostalAddress(input);
     std::cout<<"Please enter a email address: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setEmailAddress(input);
     std::cout<<"Please enter a phone nmber: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setPhoneNumber(input);
     std::cout<<"Please enter a birthday date: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setBirthdayDate(input);
     std::cout<<"Please enter a favorite meal: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setFavoriteMeal(input);
     std::cout<<"Please enter a underwear color: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setUnderwearColor(input);
     std::cout<<"Please enter a darkest secret: ";
-	std::cin>>input;
+	std::getline(std::cin, input);
 	newContact.setDarkestSecret(input);
 
 	return (newContact);
@@ -83,15 +83,15 @@ int			main() {
 	std::string input;
 	Contact phonebook[8];
 	int i = 0;
+	int index = 0;
 
 	for (;;) {
 		std::cout<<"Please enter a command: ";
-		std::cin>>input;
+		std::getline(std::cin, input);
 		if (input ==  "EXIT" && input.length() == 4) {
 			std::cout<<"Exiting..."<<std::endl;
 			return 0; 
 		} else if (input == "ADD" && input.length() == 3) {
-			std::cout<<"Adding..."<<std::endl;
 			if (i < 8) {
 				phonebook[i++] = addNewContact();
 				std::cout<<"New contact added successfully!"<<std::endl;
@@ -99,11 +99,29 @@ int			main() {
 				std::cout<<"Insufficient memory. Add failed."<<std::endl;
 			}
 		} else if (input == "SEARCH" && input.length() == 6) {
-			std::cout<<"Searching..."<<std::endl;
 			if (i == 0) {
 				std::cout<<"Phonebook is empty. Please add a contact first by using the command 'ADD'."<<std::endl;
 			} else {
 				listPhonebook(phonebook, i);
+				std::cout<<"Please select an index: ";
+				std::getline(std::cin, input);
+				std::cout<<std::endl;
+				index = input[0] - 48;
+				if (input.length() == 1 && input[0] >= '0' && input[0] <= '7' && index <= i) {
+					std::cout<<"firstName: "<<phonebook[index].getFirstName()<<std::endl;
+					std::cout<<"lastName: "<<phonebook[index].getLastName()<<std::endl;
+					std::cout<<"nickname: "<<phonebook[index].getNickname()<<std::endl;
+					std::cout<<"login: "<<phonebook[index].getLogin()<<std::endl;
+					std::cout<<"postalAddress: "<<phonebook[index].getPostalAddress()<<std::endl;
+					std::cout<<"emailAddress: "<<phonebook[index].getEmailAddress()<<std::endl;
+					std::cout<<"phoneNumber: "<<phonebook[index].getPhoneNumber()<<std::endl;
+					std::cout<<"birthdayDate: "<<phonebook[index].getBirthdayDate()<<std::endl;
+					std::cout<<"favoriteMeal: "<<phonebook[index].getFavoriteMeal()<<std::endl;
+					std::cout<<"underwearColor: "<<phonebook[index].getUnderwearColor()<<std::endl;
+					std::cout<<"darkestSecret: "<<phonebook[index].getDarkestSecret()<<std::endl;
+				} else {
+					std::cout<<"Invalid input. Search failed."<<std::endl;
+				}
 			}
 		} 
 	}
