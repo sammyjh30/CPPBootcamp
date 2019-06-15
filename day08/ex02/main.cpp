ae9b806fcb5c212e7e33a9fb9abf2d05184da94a
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-06-15 18:11:56 +0200

    Started ex02. Incomplete

--- day08/ex02/main.cpp
+++ day08/ex02/main.cpp
@@ -1,20 +1,34 @@
-int main() {
-    MutantStack<int> mstack;
-    mstack.push(5);
-    mstack.push(17);
-    std::cout << mstack.top() << std::endl;
-    mstack.pop();
-    std::cout << mstack.size() << std::endl;
-    mstack.push(3); mstack.push(5);
-    mstack.push(737); //[...] mstack.push(0);
-    MutantStack<int>::iterator it = mstack.begin();
-    MutantStack<int>::iterator ite = mstack.end();
-    ++it; 
-    --it; 
-    while (it != ite) { 
-        std::cout << *it << std::endl;
-        ++it;
-    } 
-    std::stack<int> s(mstack);
-    return 0;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/15 18:06:00 by shillebr          #+#    #+#             */
+/*   Updated: 2019/06/15 18:06:17 by shillebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mutantstack.hpp"
+
+int		main(void) {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3); mstack.push(5);
+	mstack.push(737); //[...] mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it; 
+	--it; 
+	while (it != ite) { 
+		std::cout << *it << std::endl;
+		++it;
+	} 
+	std::stack<int> s(mstack);
+	return 0;
 }
---
+++ day08/ex02/mutantstack.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shillebr <shillebr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/15 18:03:42 by shillebr          #+#    #+#             */
+/*   Updated: 2019/06/15 18:06:31 by shillebr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+#include "mutantstack.hpp"
+
+class MutantStack
+{
+	private:
+
+	public:
+};
+
+#endif
